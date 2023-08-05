2f6823e2ccf5dcd1b88defe2712e549e08035ae4
Bruhtek <bartosz.kas2005@gmail.com>
2023-08-05 18:23:42 +0200

    Improve advanced search mechanism

--- src/routes/(authed)/search/(advanced)/+page.server.ts
+++ src/routes/(authed)/search/(advanced)/+page.server.ts
@@ -1,29 +1,25 @@
-import type { Actions } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 import getSeries from '$lib/server/database/functions/getSeries';
 import getUserProtection from '$lib/Utilities/getUserProtection';
 import mapToISerie from '$lib/server/mapToISerie';
 
 export const actions = {
-	default: async ({ request, locals }) => {
-		const formData = await request.formData();
-
-		const tags = formData.getAll('tags') as string[];
-		const authors = formData.getAll('authors') as string[];
-
-		console.log(tags, authors);
-
-		if (!tags.length && !authors.length) {
-			throw redirect(302, '/search/');
-		}
-
-		const series = await getSeries(getUserProtection(locals.user ?? undefined), {
-			tagFilter: tags,
-			authorFilter: authors,
-		});
-
-		return {
-			series: series.map((s) => mapToISerie(s)),
-		};
-	},
+	default: async ({ request, locals }) => {},
 } satisfies Actions;
+
+export const load = (async ({ request, url, locals }) => {
+	const tags = url.searchParams.getAll('tags') as string[];
+	const authors = url.searchParams.getAll('authors') as string[];
+
+	const series = await getSeries(getUserProtection(locals.user ?? undefined), {
+		tagFilter: tags,
+		authorFilter: authors,
+	});
+
+	return {
+		series: series.map((s) => mapToISerie(s)),
+		selectedTags: tags,
+		selectedAuthors: authors,
+	};
+}) satisfies PageServerLoad;
--- src/routes/(authed)/search/(advanced)/+page.svelte
+++ src/routes/(authed)/search/(advanced)/+page.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-	import type { PageData, ActionData } from './$types';
+	import type { PageData } from './$types';
 	import Header from '$lib/components/Generic/Header.svelte';
 	import MultiButton from '$lib/components/Series/MultiButton.svelte';
 	import SerieContainer from '$lib/components/Series/SerieContainer.svelte';
 	import MultipleInput from '$lib/components/Generic/MultipleInput.svelte';
-	import { enhance } from '$app/forms';
 	import { Feather } from 'sveltekit-feather-icons';
 
 	export let data: PageData;
-	export let form: ActionData;
 
 	const buttons = [{ text: 'Clear', icon: 'x', href: '/search' }];
-	let formElement: HTMLFormElement;
 	let submitButton: HTMLInputElement;
 
 	const onSeachChange = () => {
@@ -22,9 +19,6 @@
 			}, 100);
 		}
 	};
-
-	let tags: string[] = [];
-	let authors: string[] = [];
 </script>
 
 <svelte:head>
@@ -32,36 +26,37 @@
 </svelte:head>
 
 <Header title="Advanced Search">
-	{#if form}
-		<MultiButton {buttons} />
-	{/if}
+	<MultiButton {buttons} />
 </Header>
 <div>
 	<form
-		method="POST"
+		data-sveltekit-keepfocus
+		data-sveltekit-replacestate
+		method="GET"
 		class="grid grid-cols-2 gap-2"
-		use:enhance={() => {
-			return async ({ update }) => {
-				await update({ reset: false });
-			};
-		}}
-		bind:this={formElement}
 	>
 		<div class="tags">
-			<MultipleInput onInput={onSeachChange} name="Tags" suggestions={data.tags}>
+			<MultipleInput
+				onInput={onSeachChange}
+				name="Tags"
+				suggestions={data.tags}
+				valueList={data.selectedTags}
+			>
 				<Feather icon="tag" classes="h-4 w-4 inline-block text-gray-400" />
 			</MultipleInput>
 		</div>
 		<div class="authors">
-			<MultipleInput onInput={onSeachChange} name="Authors" suggestions={data.authors}>
+			<MultipleInput
+				onInput={onSeachChange}
+				name="Authors"
+				suggestions={data.authors}
+				valueList={data.selectedAuthors}
+			>
 				<Feather icon="user" classes="h-4 w-4 inline-block text-gray-400" />
 			</MultipleInput>
 		</div>
 		<input type="submit" class="hidden" bind:this={submitButton} />
 	</form>
 </div>
-{#if form}
-	<SerieContainer series={form.series} />
-{:else}
-	<SerieContainer series={data.series} />
-{/if}
+
+<SerieContainer series={data.series} />
